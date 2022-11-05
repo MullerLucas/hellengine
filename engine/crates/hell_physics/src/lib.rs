012506d5c7e8b6f9e04d74cf59d598ec0b13095d
helloki <helloki@mailbox.org>
2022-11-05 22:06:09 +0100

    feat (physics): add aabb; update cargo tomls

--- engine/crates/hell_physics/src/lib.rs
+++ engine/crates/hell_physics/src/lib.rs
@@ -2,3 +2,4 @@ mod config;
 pub use config::PhysicsConfig;
 
 pub mod systems;
+pub mod collision;
--- engine/crates/hell_physics/src/systems/mod.rs
+++ engine/crates/hell_physics/src/systems/mod.rs
@@ -1,6 +1,2 @@
 mod gravity_system;
 pub use gravity_system::GravitySystem;
-
-mod collision_system;
-pub use collision_system::CollisionSystem;
-
